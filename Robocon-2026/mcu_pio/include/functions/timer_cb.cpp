50f2108a5e5389af548add6d3a1ab0ab24b84ffe
Yelluri <yelluriani@gmail.com>
2025-11-01 17:30:43 +0530

    blah

--- Robocon-2026/mcu_pio/include/functions/timer_cb.cpp
+++
@@ -1,8 +0,0 @@
-void timer_cb(){
-
-    send_data(pack_data<Lidar>(lidar,LIDAR));
-    send_data(pack_data<BnoReading>(bnoreading,BNOREADING));
-    send_data(pack_data<Pwm>(pwm, PWM));
-
-}
-BlitzTimer t1(timer_cb, 10);
--- Robocon-2026/mcu_pio/include/pinmap/PinMap_base.h
+++
@@ -1,1 +0,0 @@
-// contains pin definitions
